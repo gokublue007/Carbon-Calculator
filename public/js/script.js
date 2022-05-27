afdec5dcf1cee74fcde639c00ecba624ca104d63
ryann127 <ryann.goldberg127@gmail.com>
2022-05-26 20:39:21 -0600

    moved script.js to public folder. connected it to profile handlebars for form submission

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,22 @@
 
 const flightInput = document.getElementById('flight');
 const flightForm = document.getElementById('flight-form');
+const depInput = document.getElementById('departure')
+const desInput = document.getElementById('destination')
 
-
-const postFlight = (tripDetails) =>
+const postFlight = (newFlight) =>
   // Fetch accepts a URL and an options object where you can declare the HTTP method, the request body, and any headers.
   fetch('https://www.carboninterface.com/api/v1/estimates', {
+    method: "POST",
     headers: {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer process.env.API_KEY'
     },
-    body: {
+    body: JSON.stringify({
         "type": "flight",
         "passengers": 1,
-        "legs": [
-          {"departure_airport": "DROP VAL", "destination_airport": "DROP VAL"},
-          {"departure_airport": "DROP VAL", "destination_airport": "DROP VAL"}
-        ]
-      }
+        "legs": [ newFlight ]
+      })
   })
     .then((res) => res.json())
     .then((data) => {
@@ -34,14 +33,13 @@ flightForm.addEventListener('submit', (e) => {
 
   // Create a new review object from the input values
   const newFlight = {
-    tripName:
-    depAirport: userNameInput.value.trim(),
-    desAirport: userNameInput.value.trim(),
+    departure_airport: depInput.value.trim(),
+    destination_airport: desInput.value.trim(),
 
   };
 
   // Call our postReview method to make a POST request with our `newReview` object.
   postFlight(newFlight)
-    .then((data) => alert(`Flight added! Flight ID: ${data.body.review_id}`))
+    .then((data) => alert(`Flight added!`))
     .catch((err) => console.error(err));
 });
