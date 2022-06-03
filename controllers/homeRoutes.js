29fa076b48579563648998ab299b4944f2a1fbf9
ryann127 <ryann.goldberg127@gmail.com>
2022-06-02 19:48:21 -0600

    troubleshot a couple of different handlebars. corrected routing error in homeRoutes and got it to redirect to correct HTML.handlebars now renders correctly as it once did.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,23 +4,23 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const tripsData = await Trips.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // // Get all projects and JOIN with user data
+    // const tripsData = await Trips.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
 
-    // Serialize data so the template can read it
-    const trips = tripsData.map((trips) => trip.get({ plain: true }));
+    // // Serialize data so the template can read it
+    // const trips = tripsData.map((trips) => trip.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      trips, 
-      logged_in: req.session.logged_in 
+      // trips, 
+      // logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
